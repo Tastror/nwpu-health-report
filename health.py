fe1fd11651d837e8f8ae00b6dc7d074d1fa9674f
Tastror <yccozy2001@163.com>
2022-03-23 16:37:43 +0800

    a small change

--- health.py
+++ health.py
@@ -14,26 +14,26 @@ from webdriver_manager.chrome import ChromeDriverManager
 class trange_bar:
     def __init__(self, all_length):
         self.bar = trange(all_length, bar_format="%s{l_bar}{bar}{r_bar}%s" % (Fore.LIGHTBLUE_EX, Fore.RESET))
-        self.now_thread = None
+        self.stop = False
 
     def end(self):
-        self.now_thread.join()
+        self.stop = True
 
     def loop(self, length, times):
         for _ in range(length):
+            if self.stop: break
             time.sleep(times)
             self.bar.update(1)
 
     def update(self, length, times):
-        self.now_thread = threading.Thread(target=self.loop, args=(length, times), daemon=True)
-        self.now_thread.start()
+        threading.Thread(target=self.loop, args=(length, times), daemon=True).start()
 
 
 def health_report(username: str, password: str):
     print(time.asctime(time.localtime(time.time())))
     print("\033[1;32m开始疫情填报\033[0m")
+    bar = trange_bar(100)
     try:
-        bar = trange_bar(100)
         bar.update(35, 0.2)
         option = webdriver.ChromeOptions()
         option.add_argument("--headless")
@@ -64,26 +64,28 @@ def health_report(username: str, password: str):
         # bar.end()
         print("\033[1;32m疫情填报完成\033[0m")
     except Exception as e:
-        del bar
+        bar.end()
         print(e)
         print("\033[1;31m今日填报有误，请手动完成\033[0m")
 
 
 if __name__ == "__main__":
-    config: dict = json.load(open('./config.json', 'r', encoding='utf-8'))
-    user, passwd = config["username"], config["password"]
-    start_time = re.split("[:：.]", config.get("time", "0:30"))
-    start_time = int(start_time[0]) * 60 + int(start_time[1])
-    print("正在测试填报")
-    health_report(user, passwd)
     while True:
+
+        # 可以在运行时手动更新 config.json
+        config: dict = json.load(open('./config.json', 'r', encoding='utf-8'))
+        user, passwd = config["username"], config["password"]
+        start_time = re.split("[:：.]", config.get("time", "0:30"))
+        start_time = int(start_time[0]) * 60 + int(start_time[1])
         now_time = time.localtime(time.time())
         now_time = now_time.tm_hour * 60 + now_time.tm_min
+        health_report(user, passwd)
+
         if start_time > now_time:
             print("下次填报将等待", (start_time - now_time), "分钟\n")
             time.sleep(60 * (start_time - now_time))
         else:
             print("下次填报将等待", (start_time - now_time + 60 * 24), "分钟\n")
             time.sleep(60 * (start_time - now_time + 60 * 24))
-        health_report(user, passwd)
+
 
